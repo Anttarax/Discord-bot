4ca59f10a73b3deb8f6fa5c4e643f91f59a25583
Anttarax <vancsura.pisti@gmail.com>
2018-04-14 15:46:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,21 @@
 const Discord = require('discord.js');
-const client = New Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
-          console.log('I am ready');
+    console.log('I am ready!');
 });
-          
+
 client.on('message', message => {
-if (message.content === 'ping') {
-message.reply('pong');
-}
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
 });
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
