717d7c089da143d982eacbe37e468f3768f44dad
Anttarax <vancsura.pisti@gmail.com>
2018-04-14 15:23:22 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const Discord = New Discord.client();
+
+client.on('ready', ()=> {
+          console.log('I am ready');
+          });
+          
+client.on('message', message => {
+if (message.content === 'ping') {
+message .reply('ping');
+}
+});
+client.login(process.env.BOT_TOKEN);
